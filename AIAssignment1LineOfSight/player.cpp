1e1fb9db00c5ab86ad2876b2fbba6c39d5fee98f
Jamie <i7218850@bournemouth.ac.uk>
2015-01-27 00:47:42 +0000

    Added a Player class which inherits Creature. The Player class also contains Boolean variables for the input commands. Each one of these commands has a setter.

---
+++ AIAssignment1LineOfSight/player.cpp
@@ -0,0 +1,51 @@
+#include "player.h"
+
+/**************************************************************************************************************/
+
+/*Constructs the player object*/
+Player::Player(Texture * inTexture, Vec2 inPos, int w, int h) : Creature(inTexture, inPos, w, h)
+{
+}
+
+/**************************************************************************************************************/
+
+/*Destructs the player object*/
+Player::~Player()
+{
+}
+
+/**************************************************************************************************************/
+
+/*sets state of the up command*/
+void Player::commandUp(bool command)
+{
+	/*sets the state of the command*/
+	up = command;
+}
+
+/**************************************************************************************************************/
+
+/*sets state of the down command*/
+void Player::commandDown(bool command)
+{
+	/*sets the state of the command*/
+	down = command;
+}
+
+/**************************************************************************************************************/
+
+/*sets state of the left command*/
+void Player::commandLeft(bool command)
+{
+	/*sets the state of the command*/
+	left = command;
+}
+
+/**************************************************************************************************************/
+
+/*sets state of the right command*/
+void Player::commandRight(bool command)
+{
+	/*sets the state of the command*/
+	right = command;
+}
---
+++ AIAssignment1LineOfSight/player.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <SDL.h>
+#include "texture.h"
+#include "vec2.h"
+#include "creature.h"
+
+/**
+@brief Creates an Player object that inherits Creature.
+Creates an Player object that inherits Creature and contains the details for the Player.
+*/
+class Player : public Creature
+{
+private:
+	/**Input commands*/
+	bool up, down, left, right;
+public:
+	/**
+	Constructs a Player object
+	@param SDL_Renderer * a pointer to the renderer
+	@param Vec2 the position of the Player
+	@param int the screen width
+	@param int the screen height
+	*/
+	Player(Texture*, Vec2, int, int);
+
+	/**
+	Destructs the Player object
+	*/
+	~Player();
+
+	/**
+	Setter # Input command - Up
+	@param bool The new state of the command
+	*/
+	void commandUp(bool);
+
+	/**
+	Setter # Input command - Down
+	@param bool The new state of the command
+	*/
+	void commandDown(bool);
+
+	/**
+	Setter # Input command - Left
+	@param bool The new state of the command
+	*/
+	void commandLeft(bool);
+
+	/**
+	Setter # Input command - Right
+	@param bool The new state of the command
+	*/
+	void commandRight(bool);
+};
